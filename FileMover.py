64f95a586335f09142ff8561e5742259a20875e3
Russell LaFleur <russlafleur2@users.noreply.github.com>
2021-03-06 19:04:21 -0500

    Latest Updates - Fully Working (#2)
    * 2020 Updates - Working
    
    * Misc tweaks to improve process

--- FileMover.py
+++ FileMover.py
@@ -21,6 +21,7 @@ def main():
     for file in sys.argv[2:]:
         text = pdfToText(file)
         if text:
+            LOG.debug(text)
             processFile(file, text)
         else:
             error = 'File could not be parsed...'
@@ -31,7 +32,8 @@ def main():
 def processFile(file, text):
     # Loop through all the fileTypes in the config file to determine which type we have
     for fileProps in config["filePropertiesList"]:
-        match = re.search(re.escape(fileProps["search"]), text)
+        LOG.debug(fileProps["search"])
+        match = re.search(fileProps["search"], text, re.MULTILINE)
         if match:
             # If found fileType, now find the date
             fileDate = findDate(text, fileProps)
@@ -46,16 +48,23 @@ def processFile(file, text):
 
 
 def findDate(text, fileProps):
-    match = re.search(fileProps["dateSearch"], text)
+    match = re.search(fileProps["dateSearch"], text, re.MULTILINE)
     if match:
-        dateObj = datetime.strptime(match.group(1), fileProps["dateFormat"])
+        dateObj = tryParsingDate(match.group(1))
         return dateObj.strftime(config["finalDateFormat"])
     else:
-        error = 'Unable to find the date'
+        error = "(%s) - Unable to find the date" % (fileProps["type"])
         LOG.error(error)
         print(error)
         return None
 
+def tryParsingDate(text):
+    for searchableDateFormat in config["searchableDateFormats"]:
+        try:
+            return datetime.strptime(text, searchableDateFormat)
+        except ValueError:
+            pass
+    raise ValueError('no valid date format found')
 
 def renameFile(file, fileProps, fileDate):
     newFileName = fileProps["fileName"].replace('{DATE}', fileDate)
@@ -68,9 +77,10 @@ def renameFile(file, fileProps, fileDate):
             print(error)
             raise e
         else:
-            success = "Successfully renamed file from %s to %s" % (file, newFileName)
-            LOG.info(success)
-            print(success)
+            logSuccess = "Successfully renamed file from %s to %s" % (file, newFileName)
+            LOG.info(logSuccess)
+            printSuccess = "%s\n\nSuccessfully renamed & moved file to %s" % (fileProps["type"], newFileName)
+            print(printSuccess)
     else:
         error = "File already exists, will NOT overwrite : %s" % (newFileName)
         LOG.error(error)
@@ -80,7 +90,7 @@ def renameFile(file, fileProps, fileDate):
 # Convert PDF to text
 def pdfToText(file):
     try:
-        return subprocess.check_output(["/usr/local/bin/tika", "-v", "-t", file])
+        return subprocess.check_output(["/usr/local/bin/tika", "-t", file])
     except Exception as e:
         error = "Unable to parse text from PDF file - %s" % (file)
         LOG.exception(error)
--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Rename &amp; move PDFs based on the content inside. To be more specific, the PDF
 
 ## Requirements
 * Python 2.7.13 or greater
-* Apache Tika - http://tika.apache.org/1.14/gettingstarted.html
+* Apache Tika - https://tika.apache.org/
   * For converting the PDF to text for searching
+  * https://tika.apache.org/1.23/gettingstarted.html
+  * `brew install tika`
   
 ## Usage
 ```sh
@@ -16,9 +18,15 @@ Create a `config.json` file using the included sample as a starting point. For e
 * `"type"` - Name for the fileType
 * `"search"` - Regex for fileType identification
 * `"dateSearch"` - Regex for date identification (ex: `"Statement Date:.*(\\d+/\\d+/\\d{4})"`)
-* `"dateFormat"` - Format for the date to be used in the new file name (ex: `"%m/%d/%Y"`)
 * `"fileName"` - Destination path and name for the file - (ex: `"/Documents/Bills/Company Bill - {DATE}.pdf"`)
   * `{DATE}` will be replaced by the date found using by the `dateSearch` regex
+
+## Testing
+In order to figure out the correct values for the `search` & `dateSearch` values required for the config; you may need to test parsing the file to see how the text will be output by Tika. From the terminal, run the following command to start Tika in "GUI" mode:
+```sh
+tika -g
+```
+From here, you can drag the file and see the outputted text. It may help to copy/paste the plain text output into Sublime and test the regex. Some PDFs have weird characters that are hard to find.
 
 ## Application Usage (Drag & Drop Support)
 To enable drag & drop support, you can turn the script into an application using Script Editor (Mac OS).
